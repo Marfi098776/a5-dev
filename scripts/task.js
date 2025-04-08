c28aec4f1e7c1216f545c90ee2f117a82ed5af01
Marfi098776 <mabrurgotech@gmail.com>
2025-04-08 14:46:27 +0600

    added utilities

--- scripts/task.js
+++ scripts/task.js
@@ -2,3 +2,8 @@ document.getElementById("Discover").addEventListener("click", function(event){
     event.preventDefault();
     window.location.href = 'blog.html'
 })
+
+document.getElementById("task-btn").addEventListener("click", function(event){
+    event.preventDefault();
+    const Task = 
+})
